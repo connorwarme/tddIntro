fbb8013d68da672286a911bc428826bd39605600
connorwarme <connor.warme@gmail.com>
2022-09-28 14:43:31 -0600

    Working through testing objectives

--- src/index.js
+++ src/index.js
@@ -5,5 +5,58 @@ const capitalize = (inputString) => {
     return result;
 }
 const string = 'i want to win.';
+const reverseString = (input) => {
+    let output = [""];
+    for (let i = (input.length - 1); i>=0; i--) {
+        let letter = input.slice(i, i+1);
+        output[0] += letter;
+    }
+    return output[0].toString();
+}
+reverseString(string);
 
-export { capitalize, string };
+const objCreate = () => {
+    let add = function(a, b) { return (a+b) };
+    let sub = function(a, b) { return (a-b) };
+    let mul = function(a, b) { return (a*b) };
+    let div = function(a, b) { return (a/b) };
+    return {add, sub, mul, div}
+}
+const calculator = objCreate();
+
+const alpha = "abcdefghijklmnopqrstuvwxyz";
+
+const caesar = (string, shift) => {
+    let cipher = [""];
+    const find = (letter) => {
+        if (alpha.indexOf(letter) == -1) {
+            return -1;
+        } else {
+            return alpha.indexOf(letter);
+        }
+    }
+    const shiftLetter = (letter, index, shift) => {
+        if (index != -1) {
+            let number = index + shift;
+            if (number > 25) {
+                number = number - 26;
+            }
+            return alpha.charAt(number);
+        } else {
+            return letter;
+        }
+    }
+    for (let i = 0; i<string.length; i++) {
+        let single = string.charAt(i);
+        let index = find(single);
+        console.log(`index: ${index}`);
+        let shifted = shiftLetter(single, index, shift);
+        console.log(`shifted: ${shifted}`);
+        cipher[0] += shifted;
+    }
+    console.log(cipher[0].toString());
+    return cipher[0].toString();
+}
+
+console.log(caesar('ab.c', 3));
+export { capitalize, string, reverseString, calculator, caesar };
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,25 @@
-import { capitalize, string } from './index';
+import { 
+    capitalize, 
+    string, 
+    reverseString,
+    calculator,
+    caesar
+ } from './index';
 
 test('Capitalize first letter of string', () => {
     expect(capitalize(string)).toBe('I want to win.');
+})
+test('Reverse string!', () => {
+    expect(reverseString(string)).toBe('.niw ot tnaw i');
+})
+test('Calculator functions', () => {
+    expect(calculator.add(2, 3)).toEqual(5);
+    expect(calculator.sub(5, 6)).toEqual(-1);
+    expect(calculator.mul(5, 5)).toEqual(25);
+    expect(calculator.div(9, 3)).toEqual(3);
+})
+test('Caesar cipher attempt', () => {
+    expect(caesar('turn', 3)).toBe('wxuq');
+    expect(caesar('zero', 5)).toBe('ejwt');
+    expect(caesar('ab.c', 3)).toBe('de.f')
 })
